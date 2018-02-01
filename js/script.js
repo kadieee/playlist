72411785de6be671f4763d8ea61f1a4e735cf8ac
kadie truong <kadietruong16@yahoo.com>
2018-02-01 00:32:37 +0000

    lala

--- js/script.js
+++ js/script.js
@@ -57,18 +57,20 @@ var myPlayList = [
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
 	// everything inside this function happens as soon as the page loads!
-
-
+	displaySong(mySong);
 
 });
-
 // displaySong uses the properties in the songObject to create an HTML element for a single song
 //	  and appends the element to the playlist on the page
+
 function displaySong(songObject){
-
-
-
+    $("body").append("<div id='songInfo'></div>");
+	$("#songInfo").append("<p>" + mySong.title + "</p>");
+    $("#songInfo").append("<p>" + mySong.artist + "</p>");
+    $("#songInfo").append('<img src="' + mySong.imageURL + '"</p>');
+    $("#songInfo").append('<a href="' + mySong.playURL + '"> Play Song</a>');
 }
+
 
 // displayList takes in an array of song objects, and it uses the information from each song object
 //    to create an HTML element and append it to the playlist on the page
